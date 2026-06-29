5f7689e4620c62e41bc689a7096764bc330823ed
SageOS-RV <dev@sageos.org>
2026-06-28 23:00:47 -0400

    debug: readline echoes line-at-once, adds < > markers

--- kernel/vm/metal_rv64_vm_impl.c
+++ kernel/vm/metal_rv64_vm_impl.c
@@ -806,6 +806,8 @@ static void handle_vmsys(MetalRV64VM *vm, RV64Instruction inst) {
                         } else if (rv_strcmp(b_name, "readline") == 0 ||
                                    rv_strcmp(b_name, "input") == 0 ||
                                    rv_strcmp(b_name, "read") == 0) {
+                            // Debug: prove builtin is invoked
+                            if (vm->write_char) { vm->write_char('<'); }
                             char buf[256];
                             int pos = 0;
                             while (pos < 255) {
@@ -813,19 +815,20 @@ static void handle_vmsys(MetalRV64VM *vm, RV64Instruction inst) {
                                 if (c < 0) { __asm__ volatile("wfi"); continue; }
                                 if (c == '\n' || c == '\r') break;
                                 if (c == '\b' || c == 127) {
-                                    if (pos > 0) {
-                                        pos--;
-                                        if (vm->write_char) { vm->write_char('\b'); vm->write_char(' '); vm->write_char('\b'); }
-                                    }
+                                    if (pos > 0) { pos--; }
                                     continue;
                                 }
                                 buf[pos++] = (char)c;
-                                if (vm->write_char) vm->write_char((char)c);
                             }
                             buf[pos] = '\0';
-                            if (pos > 0 && vm->write_char) vm->write_char('\n');
+                            // Echo the whole line at once
+                            if (vm->write_char) {
+                                for (int i = 0; i < pos; i++) vm->write_char(buf[i]);
+                                vm->write_char('\n');
+                            }
                             int str_idx = rv_string_intern(vm, buf, pos);
                             vm->x[10] = (MetalValue){MV_STR, {.str_idx = str_idx}};
+                            if (vm->write_char) vm->write_char('>');
                         } else if (rv_strcmp(b_name, "SRVM") == 0) {
                             int d_idx = rv_dict_new(vm);
                             rv_dict_set(vm, d_idx,
